b780edbe243e98c8445f7600641654e92167ea94
emilie-lhomme <emilie.lhomme@sainsburys.co.uk>
2016-10-14 17:37:26 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -3,3 +3,9 @@
 xls_file = pd.ExcelFile('path')
 sheets = xls_file.sheet_names
 df = xls_file.parse(sheets[0])
+
+
+### Transform double index in 2 variables
+
+table["var1"] = (table.index.levels[0])[table.index.labels[0]]
+table["var2"] = (table.index.levels[1])[table.index.labels[1]]
