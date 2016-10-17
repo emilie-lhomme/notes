546c438f924d95c88426cb18abeb2e47a1fb977e
emilie-lhomme <emilie.lhomme@sainsburys.co.uk>
2016-10-17 16:29:56 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -9,3 +9,16 @@ df = xls_file.parse(sheets[0])
 
 table["var1"] = (table.index.levels[0])[table.index.labels[0]]
 table["var2"] = (table.index.levels[1])[table.index.labels[1]]
+
+
+### Boxplot
+
+df.boxplot(column="var_to_plot", by="group_var")
+
+
+### Draw a dendrogram
+
+from sklearn.metrics.pairwise import euclidean_distances
+from scipy.cluster import hierarchy
+Z = hierarchy.linkage(euclidean_distances(mileage), 'ward') # Can also use 'average', 'single', 'complete'...
+hierarchy.dendrogram(Z)
